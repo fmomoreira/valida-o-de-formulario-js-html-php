974338d7cbf538d78978f4b3ae7e3b63aec4cd09
Felipe Moreira <30764171+fmomoreira@users.noreply.github.com>
2020-01-02 17:26:31 -0300

    adicionando stylo no arquivo php

--- php/index.php
+++ php/index.php
@@ -4,6 +4,22 @@ $nome = $_POST['nome'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>testejs</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+</head>
+<body>
+
+<?php
 if (isset($_POST['nome'])){
 echo $nome."<br>";
 echo $email."<br>";
@@ -13,3 +29,8 @@ echo $senha."<br>";
    exit;
 }
 ?>
+
+
+</body>
+
+</html>
